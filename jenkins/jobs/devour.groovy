f983ef8dcd866f344193f050a92ede565b99cf8f
ShumzZzZz <shumin.zheng@icloud.com>
2025-08-14 09:39:59 -0700

    add groovy file to let jenkins instance initiate seed job

---
+++ jenkins/jobs/devour.groovy
@@ -0,0 +1,39 @@
+multibranchPipelineJob('Feast-CI-devour') {
+  displayName('Feast-CI-devour')
+  branchSources {
+    branchSource {
+      source {
+        github {
+          id('gh-mbp-devour')
+          repoOwner('ShuminzZzZz')
+          repository('devour')
+          credentialsId('githubapp-creds')
+          traits {
+            gitHubBranchDiscovery { strategyId(1) }
+
+            headWildcardFilter {
+              includes('main')
+              excludes('*')
+            }
+
+            gitHubPullRequestDiscovery { strategyId(1) }
+            }
+          }
+        }
+      }
+    }
+  }
+  factory {
+    workflowBranchProjectFactory {
+      scriptPath('jenkins/Jenkinsfile')
+    }
+  }
+
+  orphanedItemStrategy {
+    discardOldItems { numToKeep(5) }
+  }
+
+  triggers {
+    periodicFolderTrigger { interval('1d') }
+  }
+}

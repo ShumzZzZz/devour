3719d51df9edb787e49d6bbc39c157065c7e4873
ShumzZzZz <shumin.zheng@icloud.com>
2025-08-21 13:05:33 -0700

    streamline feast_interaction_cli.sh

--- tofu/scripts/feast_setup/interaction_remote_server_or_service/feast_interaction_cli.sh
+++ tofu/scripts/feast_setup/interaction_remote_server_or_service/feast_interaction_cli.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+export fs_external_url =
 # test pod : kubectl run curlpod --rm -it --restart=Never --image=curlimages/curl:latest -- sh
 
 curl "http://localhost:8002/get-online-features" \
@@ -24,16 +25,17 @@ curl "http://localhost:8002/get-online-features" \
 
 feast get-online-features -f fv_push_product_general_score:general_score -e product_id=9000001 -e product_id=1001
 
-curl "http://[fs-external-url]/get-online-features" \
+curl "http://${fs_external_url}/get-online-features" \
 --json '{
 		"features": ["fv_push_product_bestseller_ethnicity_tag:short_term_products_ethnic_tag"],
 		"entities": {"ethnicity_user": ["Chinese"]}
 	}' | jq
 
-curl "http://[fs-external-url]/get-online-features" --json '{"features": ["fv_push_user_propensity_score:user_type","fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502, 93590]}}' | jq
+curl "http://${fs_external_url}/get-online-features" --json '{"features": ["fv_push_user_propensity_score:user_type","fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502, 93590]}}' | jq
 
-curl "http://[fs-internal-url]/get-online-features" --json '{"features": ["fv_push_user_propensity_score:user_type","fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502, 93590]}}' | jq
+curl "http://${fs_internal_url}/get-online-features" --json '{"features": ["fv_push_user_propensity_score:user_type","fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502, 93590]}}' | jq
 
-curl "http://[fs-external-url]/get-online-features" --json '{"features": ["fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502]}}' | jq
-curl "http://[fs-internal-url]/feast/get-online-features" --json '{"features": ["fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502]}}' | jq
+curl "http://${fs_external_url}/get-online-features" --json '{"features": ["fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502]}}' | jq
+curl "http://${fs_internal_url}/feast/get-online-features" --json '{"features": ["fv_push_user_propensity_score:score"], "entities": {"user_id": [7216502]}}' | jq
 feast get-online-features -f fv_push_category_ranking:l1_all_list -e user_id=7216502
+

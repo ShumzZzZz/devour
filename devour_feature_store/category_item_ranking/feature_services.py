eee16e6433722679724a5500650eb4c36285cbd3
ShumzZzZz <shumin.zheng@icloud.com>
2025-08-26 20:51:14 -0700

    Revert "batch update the feature objects definition"
    This reverts commit 7de2301dd2e31def215ce3e20edde4800a9c33eb.

--- devour_feature_store/category_item_ranking/feature_services.py
+++ devour_feature_store/category_item_ranking/feature_services.py
@@ -18,5 +18,8 @@ fs_category_ranking_generic_v2 = FeatureService(
 )
 
 fs_test_v1 = FeatureService(
-    name="fs_test_v2", features=[fv_push_user_propensity_score]
+    name="fs_test_v2",
+    features=[
+        fv_push_user_propensity_score
+    ]
 )
